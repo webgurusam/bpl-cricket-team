bf3051b5ba71df243057ded44216784483d638f9
webgurusam <shyamghosh666@gmail.com>
2024-05-22 18:29:01 +0600

    added background image in the bpl players component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import BPL from './components/BPL/BPL'
+import BPL_Players from './components/BPL_Players/BPL_Players'
 
 function App() {
 
   return (
     <>
       <BPL></BPL>
+      <BPL_Players></BPL_Players>
     </>
   )
 }
---
+++ src/components/BPL_Players/BPL_Players.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./BPL_Players.css";
+
+const BPL_Players = () => {
+    return (
+        <div className='bpl-players-container'>
+            <h1>All The Players</h1>
+        </div>
+    );
+};
+
+export default BPL_Players;
